924b4f9a71b544f1d0f707f32e7a4aa50ebf94e6
Kishwar <kishwar.shafin@gmail.com>
2018-11-05 23:58:53 -0800

    local test off :fire:

--- parallele_test.py
+++ parallele_test.py
@@ -200,8 +200,8 @@ def chromosome_level_parallelization(chr_name,
     # if there's no confident bed provided, then chop the chromosome
     fasta_handler = FRIDAY.FASTA_handler(ref_file)
 
-    # interval_start, interval_end = (0, fasta_handler.get_chromosome_sequence_length(chr_name) + 1)
-    interval_start, interval_end = (266000, 266094)
+    interval_start, interval_end = (0, fasta_handler.get_chromosome_sequence_length(chr_name) + 1)
+    # interval_start, interval_end = (266000, 266094)
     # interval_start, interval_end = (269856, 269996)
     # interval_start, interval_end = (701150, 701170)
     # interval_start, interval_end = (260000, 260999)
