7b331c457e6ac28dd264b07692c184824f8a7237
Kishwar <kishwar.shafin@gmail.com>
2018-11-20 17:39:44 -0800

    helper update

--- modules/python/helper/csv_analyzer.py
+++ modules/python/helper/csv_analyzer.py
@@ -8,9 +8,10 @@ csv_path = sys.argv[1]
 
 tmp_df = pd.read_csv(csv_path, header=None)
 img_files = list(tmp_df[0])
+allele_path = list(tmp_df[2])
 for i in tqdm(range(len(img_files))):
-    img_file = img_files[i]
-    hdf5_file_path, allele_dict_path = img_file.split(' ')
+    hdf5_file_path = img_files[i]
+    allele_dict_path = allele_path[i]
     if os.path.isfile(hdf5_file_path) is False:
         print("INVALID FILE PATH: ", hdf5_file_path)
         exit()
@@ -19,7 +20,6 @@ for i in tqdm(range(len(img_files))):
         exit()
     else:
         hdf5_file = h5py.File(hdf5_file_path, 'r')
-
         if 'images' not in hdf5_file.keys():
-            print("NO IMAGES IN HDF5", img_file)
+            print("NO IMAGES IN HDF5", hdf5_file_path)
             exit()
