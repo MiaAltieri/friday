3d4a28d3f46afc31bd0e9e11a63adc4e4c14090e
Kishwar <kishwar.shafin@gmail.com>
2018-11-29 11:40:06 -0800

    ah, there was a bug :fire:

--- modules/src/candidate_finding/candidate_finder.cpp
+++ modules/src/candidate_finding/candidate_finder.cpp
@@ -186,15 +186,31 @@ pair<set<long long>, map<long long, PositionalCandidateRecord>  > CandidateFinde
     for(long long i=0; i < coverage.size(); i++) {
         vector< pair<double, Candidate> > positional_candidates;
 
+        int freq = 0;
+        int mismatch_count = 0;
+        for(auto& candidate: AlleleMap[i]) {
+            mismatch_count += AlleleFrequencyMap[candidate];
+        }
+
+        if(coverage[i] > 0)
+            freq = (int) ceil(100.0 *  ((double) mismatch_count / (double) coverage[i]));
+        if(freq < CandidateFinder_options::freq_threshold ||
+           mismatch_count < CandidateFinder_options::min_count_threshold){
+//            for(auto& candidate: AlleleMap[i])
+//                cout<<"SKIPPED CANDIDATE: "<<i+this->region_start<<" "<<candidate.allele.ref<<" " <<candidate.allele.alt<<" "<<candidate.allele.alt_type<<" "<<AlleleFrequencyMap[candidate]<<" "<<coverage[i] << " " <<freq<<endl;
+
+            continue;
+        }
+
         for(auto& candidate: AlleleMap[i]) {
             int freq_can = 0;
             if(coverage[i] > 0)
                 freq_can = (int)ceil(100.0 *  ((double) AlleleFrequencyMap[candidate] / (double) coverage[i]));
 
-            if(AlleleFrequencyMap[candidate] >= CandidateFinder_options::min_count_threshold &&
+            if(AlleleFrequencyMap[candidate] > CandidateFinder_options::min_count_threshold &&
                freq_can >= CandidateFinder_options::freq_threshold) {
                 filtered_candidate_positions.insert(i + this->region_start);
-                positional_candidates.push_back(make_pair(freq, candidate));
+                positional_candidates.push_back(make_pair(freq_can, candidate));
             }
 //            cout<<"CANDIDATE: "<<i+this->region_start<<" "<<candidate.allele.ref<<" "<<candidate.allele.alt<<" "<<candidate.allele.alt_type<<" "<<AlleleFrequencyMap[candidate]<<" "<<coverage[i]<<endl;
         }
