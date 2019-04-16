8e17fae451b1eae4d7b7727f94cd614bf7d828ad
Kishwar <kishwar.shafin@gmail.com>
2019-04-15 23:31:55 -0700

    natural checking on :fire:

--- modules/python/OverlappingVariantSolver.py
+++ modules/python/OverlappingVariantSolver.py
@@ -10,6 +10,33 @@ import math
 import itertools
 from modules.python.Options import VariantPostProcessingOptions
 
+"""
+https://github.com/google/deepvariant/blob/master/deepvariant/haplotypes.py
+IMPLMENTATION EDITED FROM DEEPVARIANT'S POST-PROCESSING PIPELINE
+"""
+
+
+class VariantCompatibilityCalculator(object):
+    def __init__(self, overlapping_variants):
+        min_start = min(v.pos_start for v in overlapping_variants)
+        self.variant_indices = [
+            (v.pos_start - min_start, v.pos_end - min_start) for v in overlapping_variants
+        ]
+        self.size = max(v.pos_end - min_start for v in overlapping_variants)
+
+    def all_variants_compatible(self, nonref_genotype_counts, ploidy=2):
+        if len(nonref_genotype_counts) != len(self.variant_indices):
+            raise ValueError(
+                'Variant counts must have same length as variant indices.')
+        if not all(0 <= cnt <= ploidy for cnt in nonref_genotype_counts):
+            raise ValueError('Invalid variant allele count for ploidy {}: {}'.format(
+                ploidy, nonref_genotype_counts))
+
+        alts_in_span = np.zeros(self.size, dtype=int)
+        for cnt, (start, end) in zip(nonref_genotype_counts, self.variant_indices):
+            alts_in_span[start:end] += cnt
+        return np.all(alts_in_span <= ploidy)
+
 
 class OverLappingVariantSolver:
     @staticmethod
@@ -34,13 +61,24 @@ class OverLappingVariantSolver:
 
         return all_groups
 
+    def genotype_count(self, variant):
+        return sum(g > 0 for g in variant.genotype)
+
     def solve_variant_group(self, grouped_variant):
         if len(grouped_variant) == 1:
             return grouped_variant[0]
-        else:
-            for gv in grouped_variant:
-                print(gv)
-            print("---------------------")
+
+        calculator = VariantCompatibilityCalculator(grouped_variant)
+        nonref_counts = [self.genotype_count(v) for v in grouped_variant]
+
+        if calculator.all_variants_compatible(nonref_counts):
+            return grouped_variant
+
+        print("NOT COMPATIBLE CALLS: ")
+        for variant in grouped_variant:
+            print(variant)
+        print("-----------------------------")
+
 
     def solve_overlapping_variants(self, sorted_called_variants):
         grouped_variants = self.group_variants(sorted_called_variants)
